50b5994a48afb69af372c0230a4c5bb2836fa9d0
mountkin <mountkin@gmail.com>
2015-01-19 17:45:39 +0800

    add ServerAddress field to AuthConfig struct

--- auth.go
+++ auth.go
@@ -8,9 +8,10 @@ import (
 
 // AuthConfig hold parameters for authenticating with the docker registry
 type AuthConfig struct {
-	Username string `json:"username,omitempty"`
-	Password string `json:"password,omitempty"`
-	Email    string `json:"email,omitempty"`
+	Username      string `json:"username,omitempty"`
+	Password      string `json:"password,omitempty"`
+	Email         string `json:"email,omitempty"`
+	ServerAddress string `json:"serveraddress,omitempty"`
 }
 
 // encode the auth configuration struct into base64 for the X-Registry-Auth header
